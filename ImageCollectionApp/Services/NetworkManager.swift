a9de4f30bab9eeb9e476094ae32ac97d765e123b
AlexandrMeyer <alexandrmeyerr@gmail.com>
2022-02-06 10:25:06 +0100

    fix Search

--- ImageCollectionApp/Services/NetworkManager.swift
+++ ImageCollectionApp/Services/NetworkManager.swift
@@ -82,20 +82,4 @@ class NetworkManager {
             }
         }
     }
-    
-    
-    
-    
-    func fetchPoster(from url: URL, completion: @escaping(Data, URLResponse) -> Void) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, let response = response else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            guard url.lastPathComponent == response.url?.lastPathComponent else { return }
-            DispatchQueue.main.async {
-                completion(data, response)
-            }
-        }.resume()
-    }
 }
